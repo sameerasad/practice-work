6007e27c52f1d752da55d1add44ee0ff6edcae23
sameerasad <ssameerkhan525@gmail.com>
2020-07-21 11:36:56 -0400

    add useState and counter on condition

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState}  from 'react'
+import {Message} from './Message'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+     const App = () => {
+   
+    const [count, setCount] = useState(0)
+    let [isDay, setDay] = useState(true)
+
+    return (
+        <div className={`box ${isDay ? "dayLight" : ""}`}>
+            <Message counter={count} />
+            <br/>
+            <button onClick={()=>setCount(count+1)}  >Click Me</button>
+
+
+            
+            <br/>
+            <br/>
+
+            <button onClick={()=>setCount(count-1)}>Click Me For Decrement</button>
+            <br/>
+            <br/>
+            <button onClick={()=>setDay(!isDay)}>changeColor</button>
+        </div>
+    )
 }
 
-export default App;
+export default App
