d8df9ddefc3785e53e747cf037f447180a7fdd8a
sameerasad <ssameerkhan525@gmail.com>
2020-07-22 11:36:57 -0400

    some chage

--- src/Message.js
+++ src/Message.js
@@ -4,7 +4,8 @@ export const Message = (props) => {
     return (
         <div>
             <h3>value of count={props.counter}</h3>
-            <h3>value of count-{props.count}</h3>
+            
+
         </div>
     )
 }
