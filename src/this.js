b317b08a27a887c2339cee4de793741b22094f88
sameerasad <ssameerkhan525@gmail.com>
2020-07-27 11:19:54 -0400

    add about the result which we get after logging the this into console

--- src/this.js
+++ src/this.js
@@ -1,7 +1,9 @@
-this keyword  is different and in javascript in considered a notorious thing here
+this keyword  is different and in javascript and considered a notorious thing here
 because it is quite difficul to understand and it could be differnt for different scopes
 
 //just log this keyword on console
 
 
-console.log(this); 
+console.log(this); // it will give us the global context
+
+//ans[object Window]
