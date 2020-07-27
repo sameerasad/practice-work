6bf4a493a2b8a257a967c9776cfecbc82151bdda
sameerasad <ssameerkhan525@gmail.com>
2020-07-27 11:15:26 -0400

    add some detailes

---
+++ src/this.js
@@ -0,0 +1,7 @@
+this keyword  is different and in javascript in considered a notorious thing here
+because it is quite difficul to understand and it could be differnt for different scopes
+
+//just log this keyword on console
+
+
+console.log(this); 
