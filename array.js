83a22211d2d021cb8f05fa9eeddedddb8b57aa6c
sameerasad <ssameerkhan525@gmail.com>
2020-07-28 11:00:25 -0400

    add some description about declaration

--- array.js
+++ array.js
@@ -4,4 +4,10 @@ in java script array is the one of  the most important data structure
 
 we can read  documentation of it MDN 
 
+declaration
+
+let countries= [];//[]these brackets shows that this is array
+
+
+
  
