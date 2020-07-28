8910888e19ef585c16a4c5466460c287cd899713
sameerasad <ssameerkhan525@gmail.com>
2020-07-28 11:07:44 -0400

    add some description about what we can store in it

--- array.js
+++ array.js
@@ -12,6 +12,10 @@ countries=["Pakistan","India","England"];
 
 //we could put multiple values in array of having any data types means we can store hetrogenius data in in javascript array
 
+const hetrogeniusArray =["Pakistan",1,"india",true]
+
+// we store the string number and booolean dataType in above array and it will work as well.
+
 
 
  
