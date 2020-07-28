dfaf3400ea39c591ae6cd927e3c6c21bbebc40f4
sameerasad <ssameerkhan525@gmail.com>
2020-07-28 11:03:54 -0400

    add some description about what we can store in it

--- array.js
+++ array.js
@@ -8,6 +8,10 @@ declaration
 
 let countries= [];//[]these brackets shows that this is array
 
+countries=["Pakistan","India","England"];
+
+//we could put multiple values in array of having any data types means we can store hetrogenius data in in javascript array
+
 
 
  
